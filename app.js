393aac4bbb4004b37c16261a9a1cce2b32ae8a23
CodeWhite <hashirabbasi264@gmail.com>
2025-08-25 14:12:46 -0700

    new

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
 const express = require('express');
 const app = express();
-const hostname = '127.0.0.1'; // Your server ip address
 const port = 3000;
 
 const version = '1.0.0';
 
 app.get('/', (req, res) => {
-    // set response content    
-    res.sendFile(__dirname + "/html/index.html"); 
-    console.log(`[Version ${version}]: New request => http://${hostname}:${port}`+req.url);
-
-})
+    res.sendFile(__dirname + "/html/index.html");
+    console.log(`[Version ${version}]: New request => ${req.url}`);
+});
 
 // Health check
-app.get('/health', (req, res) => {    
+app.get('/health', (req, res) => {
     res.sendStatus(200);
-    console.log(`[Version ${version}]: New request => http://${hostname}:${port}`+req.url);
-})
+    console.log(`[Version ${version}]: Health check`);
+});
 
-app.listen(port, () => {
-    console.log(`[Version ${version}]: Server running at http://${hostname}:${port}/`);
-})
+app.listen(port, '0.0.0.0', () => {
+    console.log(`[Version ${version}]: Server running at http://0.0.0.0:${port}/`);
+});
